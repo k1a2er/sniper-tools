0ab35d70c2ce1b898272a4c65af2572041a0c360
sniper.9nine <71948239+k1a2er@users.noreply.github.com>
2020-12-13 22:23:08 +0100

    Add files via upload

---
+++ sniper-gadgets.py
@@ -0,0 +1,204 @@
+import os,sys,time
+#sys1
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(00000.02)
+os.system('clear')
+
+#this logo
+logo = ("""\033[1;32m──────────────────────────────────────────────────────────────────────────────────
+\033[1;32m                           ........
+                            ;::;;::;,
+                            ;::;;::;;,
+                           ;;:::;;::;;,
+           .vnmmnv%vnmnv%,.;;;:::;;::;;,  .,vnmnv%vnmnv,
+        vnmmmnv%vnmmmnv%vnmmnv%;;;;;;;%nmmmnv%vnmmnv%vnmmnv
+      vnmmnv%vnmmmmmnv%vnmmmmmnv%;:;%nmmmmmmnv%vnmmmnv%vnmmmnv
+     vnmmnv%vnmmmmmnv%vnmmmmmmmmnv%vnmmmmmmmmnv%vnmmmnv%vnmmmnv
+    vnmmnv%vnmmmmmnv%vnmmmmmmmmnv%vnmmmmmmmmmmnv%vnmmmnv%vnmmmnv
+  vnmmnv%vnmmmmmnv%vnmm;mmmmmmnv%vnmmmmmmmm;mmnv%vnmmmnv%vnmmmnv,
+  vnmmnv%vnmmmmmnv%vnmm;' mmmmmnv%vnmmmmmmm;' mmnv%vnmmmnv%vnmmmnv
+  vnmmnv%vnmmmmmnv%vn;;    mmmmnv%vnmmmmmm;;    nv%vnmmmmnv%vnmmmnv
+ vnmmnv%vnmmmmmmnv%v;;      mmmnv%vnmmmmm;;      v%vnmmmmmnv%vnmmmnv
+ vnmmnv%vnmmmmmmnv%vnmmmmmmmmm;;       mmmmmmmmmnv%vnmmmmmmnv%vnmmmnv
+ vnmmnv%vnmmmmmmnv%vnmmmmmmmmmm;;     mmmmmmmmmmnv%vnmmmmmmnv%vnmmmnv
+ vnmmnv%vnmmmmm nv%vnmmmmmmmmmmnv;, mmmmmmmmmmmmnv%vn;mmmmmnv%vnmmmnv
+ vnmmnv%vnmmmmm  nv%vnmmmmmmmmmnv%;nmmmmmmmmmmmnv%vn; mmmmmnv%vnmmmnv
+ `vnmmnv%vnmmmm,  v%vnmmmmmmmmmmnv%vnmmmmmmmmmmnv%v;  mmmmnv%vnnmmnv'
+  vnmmnv%vnmmmm;,   %vnmmmmmmmmmnv%vnmmmmmmmmmnv%;'   mmmnv%vnmmmmnv
+   vnmmnv%vnmmmm;;,   nmmm;'              mmmm;;'    mmmnv%vnmmmmnv'
+   `vnmmnv%vnmmmmm;;,.         mmnv%v;,            mmmmnv%vnmmmmnv'
+    `vnmmnv%vnmmmmmmnv%vnmmmmmmmmnv%vnmmmmmmnv%vnmmmmmnv%vnmmmmnv'
+      `vnmvn%vnmmmmmmnv%vnmmmmmmmnv%vnmmmmmnv%vnmmmmmnv%vnmmmnv'
+          `vn%vnmmmmmmn%:%vnmnmmmmnv%vnmmmnv%:%vnmmnv%vnmnv'
+                          \033[1;31msniper-gadgets
+──────────────────────────────────────────────────────────────────────────────""")
+#my logo
+print(logo)
+jalan("###############################################################################")
+print("")
+print("\033[1;32mPlease don't forget to subscribe to my YouTube channel to learn all about hackers, penetration and protection")
+time.sleep(6)
+
+os.system('xdg-open https://www.youtube.com/c/sniper9nine')
+print("\033[1;31m{\033[1;33mmy youtube channel: sniper.9nine\033[1;31m}")
+print("{\033[1;33mmy github account : k1a2er\033[1;31m}")
+print("{\033[1;33mmy instagram sniper.9nine\033[1;31m}")
+print("{\033[1;33mmy facebook kader demon\033[1;31m}")
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(00000.08)
+jalan ("                       \033[1;31mThank you for using my tool            ")
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(00000.02)
+
+#welcom
+jalan ("\033[1;31m -_-_-__-_-_-_-_-_-_-_-_-_-_-_&&&\033[1;38mHello in my World\033[1;31m&&&_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
+jalan("_____________((\033[1;38mThis̲ tool for Hassi Abd el Kader\033[1;31m))___________")
+jalan("__________________((\033[1;38mAge :̲ 14years\033[1;31m))________________")
+jalan("____________((\033[1;38mFrom \033[1;31m☪ \033[1;32mAlgeria))________")
+print ("\033[1;31m {0}  \033[1;32m exit")
+print ("\033[1;31m {1}  \033[1;32m Download metasploit ")
+print ("\033[1;31m {2}  \033[1;32m Download All Pkg ")
+print ("\033[1;31m {3}  \033[1;32m Download TmX-FW ")
+print ("\033[1;31m {4}  \033[1;32m Download kalinethunter ")
+print ("\033[1;31m {5}  \033[1;32m Downloadّ BluForce-FB ")
+nmb = input("\033[1;32mChoose number : ")
+if nmb=="0":
+    os.system('exit')
+    os.system('clear')
+#Download Metasploit
+elif nmb=="1":
+    os.system('pkg install update && pkg install upgrade -y ')
+    os.system('pkg install unstable-repo -y ')
+    os.system('pkg install metasploit -y ')
+    os.system('clear')
+    jalan("\033[1;31m                 Download metasploit It's finished Thank you for using my tool              ")
+    os.system('msfconsole')
+
+
+
+#Download All Pkg
+elif nmb=="2":
+    os.system('apt update && apt upgrade -y ')
+    os.system('pkg update && upgrade ')
+    os.system('pkg install git -y' )
+    os.system('pkg install python -y ')
+    os.system('pkg install python2 -y ')
+    os.system('pkg install unzip -y ')
+    os.system('pkg install nano -y ')
+    os.system('pkg install cat -y ')
+    os.system('pkg install curl -y ')
+    os.system('pkg install wget -y ')
+    os.system('pkg install w3m -y ')
+    os.system('pkg install php -y ')
+    os.system('pkg install nodejs -y ')
+    os.system('apt install golang -y ')
+    os.system('apt install host -y ')
+    os.system('apt install havij -y ')
+    os.system('pkg install figlet -y ')
+    os.system('figlet Sniper-gadgets ')
+    os.system('pkg install wget -y ')
+    os.system('pkg install cfengine -y ')
+    os.system('pkg install db -y ')
+    os.system('pkg install w3m -y ')
+    os.system('pkg install netpb -y ')
+    os.system('pkg install postgresql -y ')
+    os.system('pkg install uftrace -y ')
+    os.system('pkg install python -y ')
+    os.system('pkg install python2 -y ')
+    os.system('pkg install python3 -y ')
+    os.system('pkg install java -y ')
+    os.system('pip install pyfiglet -y ')
+    os.system('pkg install fish -y ')
+    os.system('pkg install ruby -y ')
+    os.system('pip install --upgrade pip ')
+    os.system('pkg install host -y ')
+    os.system('pkg install php -y ')
+    os.system('pkg install perl -y ')
+    os.system('pkg install nmap -y ')
+    os.system('pkg install bash -y ')
+    os.system('pkg install clang -y ')
+    os.system('pkg install w3m -y ')
+    os.system('pkg install havij -y ')
+    os.system('pip install --upgrade pip ')
+    os.system('pkg install figlet -y ')
+    os.system('pkg install cowsay -y ')
+    os.system('pkg install curl -y ')
+    os.system('pkg install tar -y ')
+    os.system('pkg install zip -y ')
+    os.system('pkg install unzip -y ')
+    os.system('pip install --upgrade pip ')
+    os.system('pkg install tor -y ')
+    os.system('pkg install google -y ')
+    os.system('pkg install sudo -y ')
+    os.system('pkg install wget -y ')
+    os.system('pkg install wireshark -y ')
+    os.system('pkg install wgetrc -y ')
+    os.system('pkg install wcalc -y ')
+    os.system('pkg install bmon -y ')
+    os.system('pkg install unrar -y ')
+    os.system('pkg install toilet -y ')
+    os.system('pkg install proot -y ')
+    os.system('pkg install net-tools -y ')
+    os.system('pkg install golang -y ')
+    os.system('pip install chroot -y ')
+    os.system('pkg install openssl -y ')
+    os.system('pkg install cmatrix -y ')
+    os.system('pkg install openssh ')
+    os.system('pip install --upgrade pip ')
+    os.system('pkg install macchanger')
+    os.system('pip install colorama ')
+    os.system('pip2 install mechanize ')
+    os.system('pip2 install requests ')
+    os.system('pip install --upgrade pip ')
+    os.system('pkg install root-repo -y ')
+    os.system('pkg install unstable-repo -y ')
+    os.system('pkg install x11-repo -y ')
+    os.system('clear ')
+    os.system('apt update && apt upgrade -y ')
+    os.system('clear ')
+    os.system('figlet sniper-gadgets ')
+    time.sleep(000.5)
+    os.system('clear ')
+    jalan ("\033[1;31m             Download All Pkg It's finished Thank you for using my tool       ")
+
+#Download TmX-FW
+elif nmb=="3":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/MoMuilWH/TmX-FW ')
+    os.system('cd TmX-FW ')
+    os.system('chmod 777 TmX-FW.py ')
+    os.system('python2 TmX-FW.py ')
+    jalan("\033[1;31m                Download TmX-FW It's finished Thank you for using my tool         ")
+
+
+#Download kalinthunter
+elif nmb=="4":
+    os.system('apt update && apt upgrade')
+    os.system('pkg install wget ')
+    os.system('wget -O install-nethunter-termux https://offs.ec/2MceZWr ')
+    os.system('nethunter -r ')
+    os.system('apt update && apt upgrade -y && apt dist-upgrade -y ')
+    os.system('apt install kali-linux-default ')
+    jalan ("\033[1;31m                Download kalinethunter It's finished Thank you for using my tool           ")
+#Download BluForce-FB
+elif nmb=="5":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/AngelSecurityTeam/BluForce-FB ')
+    os.system('cd BluForce-FB ')
+    os.system('chmod 777 bluforcefb.py ')
+    os.system('python2 bluforcefb.py ')
+    jalan ("\033[1;31m                  Download BluForce-FB It's finished Thank you for using my tool               ")
+#Try ouvert my tool
+else :
+    print ("\033[1;31m                  X_False number try again_X         ")
+    time.sleep(00000.10)
+    os.system('python sniper-gadgets.py ')
